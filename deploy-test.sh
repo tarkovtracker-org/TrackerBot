a24698eae891fc575720b54cbb90e633cf750ed6
Niv <148060940+Nivmizz7@users.noreply.github.com>
2025-12-17 17:29:59 +0100

    Add deployment script for TrackerBot-test
    Signed-off-by: Niv <148060940+Nivmizz7@users.noreply.github.com>

---
+++ deploy-test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+echo "Deploy TrackerBot-test"
+
+cd "$(dirname "$0")"
+
+echo "Update"
+git fetch origin
+git checkout prod
+git pull origin prod
+
+echo "Install deps"
+npm install
+
+echo "PM2 reload"
+pm2 start ecosystem.config.cjs || true
+pm2 reload ecosystem.config.cjs --update-env
+
+echo "Save PM2 state"
+pm2 save
+
+echo "Deploy finish"
