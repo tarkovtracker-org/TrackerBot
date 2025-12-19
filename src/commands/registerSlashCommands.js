cb4ca54ec17bb5e2d37fe369176c8de211b63800
Niv <eliotley06@gmail.com>
2025-12-19 15:32:29 +0100

    Extract slash command registration

---
+++ src/commands/registerSlashCommands.js
@@ -0,0 +1,32 @@
+import { SlashCommandBuilder, Routes } from "discord.js";
+import { REST } from "@discordjs/rest";
+
+const slashCommands = [
+  new SlashCommandBuilder()
+    .setName("message")
+    .setDescription("Send a message as the bot (Discord Admin only)")
+    .addStringOption(opt =>
+      opt.setName("content")
+        .setDescription("Message content (use \\n for new lines)")
+        .setRequired(true)
+    ),
+  new SlashCommandBuilder()
+    .setName("faq1")
+    .setDescription("Post the FAQ notice about site stability"),
+  new SlashCommandBuilder()
+    .setName("utd")
+    .setDescription("Post the update-in-progress notice"),
+  new SlashCommandBuilder()
+    .setName("ticket-close")
+    .setDescription("Close this ticket and move it to Archive")
+];
+
+const slashCommandData = slashCommands.map(command => command.toJSON());
+
+export async function registerSlashCommands(client) {
+  const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
+
+  await rest.put(Routes.applicationCommands(client.user.id), {
+    body: slashCommandData
+  });
+}
